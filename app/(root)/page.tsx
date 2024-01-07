68545191f73dd5d7c8d90e9a95a4c2221beb5f38
StephenBernard98 <stephenbenard3@gmail.com>
2024-01-07 14:00:27 +0100

    fixed some bugs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,50 @@
-import EventCategory from "../event_category/page";
+import Search from "@/components/shared/Search";
 import Hero from "../hero/page";
+import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
+import CategoryFilter from "@/components/shared/CategoryFilter";
 
-export default function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
+
+  const events = await getAllEvents({
+    query: searchText,
+    category: category,
+    page: page,
+    limit: 6,
+  });
   return (
     <main className="">
       <Hero />
-      <EventCategory />
+
+      <div className=" w-full font-bold text-center text-xl md:text-4xl mt-7  gap-5 md:flex-row">
+        Search and filter Categories
+      </div>
+      <Search />
+      <CategoryFilter />
+      <section
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+        id="events"
+      >
+        <h2 className="h2-bold text-center">
+          Trusted <span className="text-[#705CF7]">by</span>
+          <br />
+          <span className="text-[#705CF7]">Thousands</span> of Events
+        </h2>
+
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
+      </section>
     </main>
   );
 }
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,8 +1,8 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
-// import { getOrdersByUser } from "@/lib/actions/order.actions";
-// import { IOrder } from "@/lib/database/models/order.model";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/mongodb/database/models/order.model";
 import { SearchParamProps } from "@/types";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
@@ -12,13 +12,13 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
-//   const ordersPage = Number(searchParams?.ordersPage) || 1;
-//   const eventsPage = Number(searchParams?.eventsPage) || 1;
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
-//   const orders = await getOrdersByUser({ userId, page: ordersPage });
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
 
-//   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
-   const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+   const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
   return (
     <>
@@ -33,7 +33,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection
+        <Collection
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore!"
@@ -42,7 +42,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           page={ordersPage}
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
-        /> */}
+        />
       </section>
 
       {/* Events Organized */}
@@ -62,7 +62,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
         />
